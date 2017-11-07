23e24835ab326ee73e758d679f5de4d349cbcb9e
Instillingererno <sveinungskole@gmail.com>
2017-11-07 10:16:32 +0100

    woooo

--- ElectronTest/Scripts/objects_static.js
+++ ElectronTest/Scripts/objects_static.js
@@ -22,3 +22,30 @@ settings.apply = () => {
     document.getElementsByClassName('tools')[0].style.background = settings.tools_background;
 
 };
+
+let budget = [
+    ["Måneder","Inntekt","Utgift"],
+    ["Januar",3000,3000],
+    ["Februar",3000,3000],
+    ["Mars",3000,3000],
+    ["April",3000,3000],
+    ["Mai",3000,3000],
+    ["Juni",3000,3000],
+    ["Juli",3000,3000],
+    ["August",3000,3000],
+    ["September",3000,3000],
+    ["Oktober",3000,3000],
+    ["November",3000,3000],
+    ["December",3000,3000]
+];
+budget.write = () => {
+    let output = "";
+    for(var i = 0; i < budget.length; i++) {
+        output += "<tr>";
+        for(var j = 0; j < budget[i].length; j++) {
+            output += (i!=0) ? '<td>' + (budget[i][j]).toString() + '</td>' : '<th>' + (budget[i][j]).toString() + '</th>';
+        }
+        output += "</tr>";
+    }
+    return output;
+}
