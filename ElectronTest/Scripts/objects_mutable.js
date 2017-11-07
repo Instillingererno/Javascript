28277eb584aaf63370432b5076ccd26c418de58f
Instillingererno <sveinungskole@gmail.com>
2017-11-07 08:52:22 +0100

    dfsd

--- ElectronTest/Scripts/objects_mutable.js
+++ ElectronTest/Scripts/objects_mutable.js
@@ -5,3 +5,23 @@ class Page {
         this.method = method;
     }
 }
+
+
+class Info {
+    constructor(melding) {
+        this.melding = melding;
+        this.visible = false;
+    }
+    show(e) {
+        let temp = document.createElement("div"); //v, {"id" : "infoblock", "class" : "info"}, this.melding
+        temp.setAttribute("class", "info");
+        temp.setAttribute("id", "infoblock");
+        temp.appendChild(document.createTextNode(this.melding));
+        document.getElementById("container").appendChild(temp);
+        let temp2 = 'right: ' + (window.innerWidth - e.clientX) + 'px; top:  ' + e.clientY + 'px;';
+        temp.setAttribute("style", temp2);
+    }
+    remove(e) {
+        document.getElementById("container").removeChild(document.getElementById("infoblock"));
+    }
+}
